3fdd62eb48a000ee4d08d57e858fa0f082e29d8e
diadestiny <1247889154@qq.com>
2024-04-02 21:34:24 +0800

    add libevent test

---
+++ test/build/CMakeFiles/fiber_lib.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fd_manager.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fd_manager.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fd_manager.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fiber.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fiber.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/fiber.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/hook.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/hook.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/hook.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/iomanager.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/iomanager.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/iomanager.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/scheduler.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/scheduler.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/scheduler.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/thread.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/thread.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/thread.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/timer.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/timer.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/timer.o.d"
+  "/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/utils.cpp" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/utils.o" "gcc" "CMakeFiles/fiber_lib.dir/data_ssd2/linkaihao/work_project/cpp_project/KVstorageBaseRaft-cpp/src/fiber/utils.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- test/test_tcp_server.cc
+++ test/test_tcp_server.cc
@@ -1,8 +1,9 @@
 // ##控制不同服务器的开关宏
-#define SERVER_1
+#define SERVER_3
 
-#ifdef  SERVER_1
+#ifdef  SERVER_1 //原生
 #include <cstdio>
+#include <iostream>
 #include <cstring>
 #include <netinet/in.h>
 #include <sys/epoll.h>
@@ -17,52 +18,51 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_len = sizeof(client_addr);
     struct epoll_event events[MAX_EVENTS], event;
+
     // 创建监听套接字
-    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
         perror("socket");
-        return -1;
-    }
+
     // 设置端口重用
     const int opt = 1;
-    setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-    // 设置服务器地址和端⼝
+    if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
+        perror("setsockopt");
+
+    // 设置服务器地址和端口
     memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(PORT);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    // 绑定监听套接字到服务器地址和端⼝
-    if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+
+    // 绑定监听套接字到服务器地址和端口
+    if (bind(listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
         perror("bind");
-        return -1;
-    }
+
     // 监听连接
-    if (listen(listen_fd, 5) == -1) {
+    if (listen(listen_fd, 5) == -1)
         perror("listen");
-        return -1;
-    }
+
     // 创建 epoll 实例
-    if ((epoll_fd = epoll_create1(0)) == -1) {
+    if ((epoll_fd = epoll_create1(0)) == -1)
         perror("epoll_create1");
-        return -1;
-    }
-    // 添加监听套接字到 epoll 实例中
-    event.events  = EPOLLIN;
+
+    // 添加监听套接字到 epoll 实例中, 默认水平触发
+    event.events  = EPOLLIN; 
     event.data.fd = listen_fd;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event) == -1) {
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listen_fd, &event) == -1)
         perror("epoll_ctl");
-        return -1;
-    }
+
     while (true) {
-        // 等待事件发⽣
+        // 等待事件发生
         event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
-        if (event_count == -1) {
-            perror("epoll_wait");
+        if (event_count == -1){
+            perror("socket");
             return -1;
         }
         // 处理事件
         for (int i = 0; i < event_count; i++) {
+            // 如果事件对应的文件描述符是监听套接字，表示有新连接到达
             if (events[i].data.fd == listen_fd) {
-                // 有新连接到达
                 conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &addr_len);
                 if (conn_fd == -1) {
                     perror("accept");
@@ -71,21 +71,19 @@ int main() {
                 // 将新连接的套接字添加到 epoll 实例中
                 event.events  = EPOLLIN;
                 event.data.fd = conn_fd;
-                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn_fd, &event) == -1) {
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn_fd, &event) == -1)
                     perror("epoll_ctl");
-                    return -1;
-                }
             } else {
-                // 有数据可读
+                // 如果事件对应的文件描述符不是监听套接字，表示有数据可读
                 char buf[1024];
                 int len = read(events[i].data.fd, buf, sizeof(buf) - 1);
                 if (len <= 0) {
-                    // 发⽣错误或连接关闭，关闭连接
-                    // printf("error,fd: %d\n",events[i].data.fd );
+                    // 发生错误或连接关闭，关闭连接
                     close(events[i].data.fd);
                 } else {
                     buf[len] = '\0';
-                    // printf("接收到消息：%s\n", buf);
+                    // 处理接收到的消息
+                    std::cout << "接收到消息：" << buf << std::endl;
                     // 发送回声消息给客户端
                     // write(events[i].data.fd, buf, len);
                     close(events[i].data.fd);
@@ -103,7 +101,7 @@ int main() {
 
 #ifdef SERVER_2
 
-#include "sylar/sylar.h"
+#include "../src/fiber/include/monsoon.h"
 #include <cstdio>
 #include <cstring>
 #include <netinet/in.h>
@@ -119,65 +117,82 @@ void error(const char *msg) {
     exit(1);
 }
 void watch_io_read() {
-    sylar::IOManager::GetThis()->addEvent(sock_listen_fd, sylar::IOManager::READ, test_accept);
+    monsoon::IOManager::GetThis()->addEvent(sock_listen_fd, monsoon::Event::READ, test_accept);
 }
+// 接受连接的函数。
 void test_accept() {
-    struct sockaddr_in addr; // maybe sockaddr_un;
-    memset(&addr, 0, sizeof(addr));
-    socklen_t len = sizeof(addr);
-    int fd        = accept(sock_listen_fd, (struct sockaddr *)&addr, &len);
+    struct sockaddr_in addr; // 可能是sockaddr_un结构，用于UNIX域套接字。
+    memset(&addr, 0, sizeof(addr)); // 清零地址结构体。
+    socklen_t len = sizeof(addr); // 地址结构体的大小。
+    int fd = accept(sock_listen_fd, (struct sockaddr *)&addr, &len); // 接受连接。
     if (fd < 0) {
+        // 接受失败时，输出错误。
         std::cout << "fd = " << fd << "accept false" << std::endl;
     } else {
+        // 设置文件描述符为非阻塞模式。
         fcntl(fd, F_SETFL, O_NONBLOCK);
-        sylar::IOManager::GetThis()->addEvent(fd, sylar::IOManager::READ, [fd]() {
+        // 为新接受的连接添加一个读取事件。
+        monsoon::IOManager::GetThis()->addEvent(fd, monsoon::READ, [fd]() {
             char buffer[1024];
-            memset(buffer, 0, sizeof(buffer));
+            memset(buffer, 0, sizeof(buffer)); // 清零缓冲区。
             while (true) {
+                // 接收数据。
                 int ret = recv(fd, buffer, sizeof(buffer), 0);
                 if (ret > 0) {
-                    // printf("recv:%s\n", buffer);
+                    // 如果接收到数据，就将其发送回客户端。
                     ret = send(fd, buffer, ret, 0);
                 }
                 if (ret <= 0) {
-                    if (errno == EAGAIN)
+                    // 如果recv返回0，表示客户端已关闭连接；如果返回负值，表示出错。
+                    if (errno == EAGAIN) // 如果错误是EAGAIN，则继续循环。
                         continue;
-                    break;
+                    break; // 否则退出循环。
                 }
-                // printf("close fd:%d\n", fd); //短连接，一次处理完毕之后，无论如何，关闭fd
+                // 关闭文件描述符，因为这是一个短连接服务器，处理完即关闭。
                 close(fd);
             }
         });
     }
-    sylar::IOManager::GetThis()->schedule(watch_io_read);
+    // 重新调度watch_io_read函数，以便继续监听新的连接请求。
+    monsoon::IOManager::GetThis()->scheduler(watch_io_read);
 }
+
 void test_iomanager() {
-    int portno = 12345;
+    int portno = 12345; // 服务器监听的端口号。
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
-    // setup socket
+    // 创建监听套接字。
     sock_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_listen_fd < 0) {
+        // 创建套接字失败时，输出错误并退出。
         const char *msg = "Error creating socket..";
         error(msg);
     }
     int yes = 1;
-    // lose the pesky "address already in use" error message
+    // 设置套接字选项，避免地址已被使用的错误。
     setsockopt(sock_listen_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
-    memset((char *)&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family      = AF_INET;
-    server_addr.sin_port        = htons(portno);
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    // bind socket and listen for connections
+
+    memset((char *)&server_addr, 0, sizeof(server_addr)); // 清零地址结构体。
+    server_addr.sin_family = AF_INET; // 设置地址类型为IPv4。
+    server_addr.sin_port = htons(portno); // 设置端口号。
+    server_addr.sin_addr.s_addr = INADDR_ANY; // 设置IP地址为任意。
+
+    // 绑定套接字到地址。
     if (bind(sock_listen_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
         error("Error binding socket..\n");
+    // 监听套接字。
     if (listen(sock_listen_fd, 1024) < 0) {
         error("Error listening..\n");
     }
+    // 输出服务器启动信息。
     printf("epoll echo server listening for connections on port: %d\n", portno);
+
+    // 将监听套接字设置为非阻塞模式。
     fcntl(sock_listen_fd, F_SETFL, O_NONBLOCK);
-    sylar::IOManager iom;
-    iom.addEvent(sock_listen_fd, sylar::IOManager::READ, test_accept);
+    // 创建IOManager实例。
+    monsoon::IOManager iom;
+    // 添加一个读取事件，以便当有新连接时可以调用test_accept。
+    iom.addEvent(sock_listen_fd, monsoon::READ, test_accept);
 }
 
 int main(int argc, char *argv[]) {
@@ -192,57 +207,122 @@ int main(int argc, char *argv[]) {
  /**
  /**
   * @file test_tcp_server.cc
-  * @brief TcpServer类测试
+  * @brief 基于libevent库实现tcp_server
   * @version 0.1
-  * @date 2021-09-18
+  * @date 2024-04-02
   */
- #include "sylar/sylar.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
- // static sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+#include <event2/event.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
 
- /**
-  * @brief 自定义TcpServer类，重载handleClient方法
-  */
- class MyTcpServer final: public sylar::TcpServer {
- protected:
-     void handleClient(sylar::Socket::ptr client) override;
- };
+#define LISTEN_PORT 8000
+#define LIATEN_BACKLOG 32
 
- void MyTcpServer::handleClient(sylar::Socket::ptr client) {
-     // SYLAR_LOG_INFO(g_logger) << "new client: " << client->toString();
-     // static std::string buf;
-     // buf.resize(4096);
-     char buf[1024];
-     client->recv(&buf[0], sizeof(buf)-1); // 这里有读超时，由tcp_server.read_timeout配置项进行配置，默认120秒
-     // SYLAR_LOG_INFO(g_logger) << "recv: " << buf;
-     client->close();
- }
+/*********************************************************************************
+*                   函数声明
+**********************************************************************************/
+//accept回调函数
+void do_accept_cb(evutil_socket_t listener, short event, void *arg);
+//read 回调函数
+void read_cb(struct bufferevent *bev, void *arg);
+//error回调函数
+void error_cb(struct bufferevent *bev, short event, void *arg);
+/*                   函数体
+**********************************************************************************/
+//accept回调函数
+void do_accept_cb(evutil_socket_t listener, short event, void *arg)
+{
+  //传入的event_base指针
+  struct event_base *base = (struct event_base*)arg;
+  
+  struct sockaddr_in sin; //声明地址
+  socklen_t slen = sizeof(sin);  //地址长度声明
+  
+  //接收客户端，并返回新的套接字conn_fd用于通讯
+  evutil_socket_t  conn_fd= accept(listener, (struct sockaddr *)&sin, &slen);
+  if (conn_fd< 0)
+  {
+    perror("error accept");
+    return;
+  }
+  printf("new client[%u]  connect success \n", conn_fd);
+  
+  //使用新的连接套接字conn_fd建立bufferevent，并将bufferevent也安插到base上
+  struct bufferevent *bev = bufferevent_socket_new(base, conn_fd, BEV_OPT_CLOSE_ON_FREE);
+  bufferevent_setcb(bev, read_cb, NULL, error_cb, arg); //设置回调函数read_cb \ error_cb
+  bufferevent_enable(bev, EV_READ | EV_PERSIST);//启用永久read/write
+  //bufferevent_enable(bev,EV_WRITE); //默认情况下，EV_WRITE是开启的
+}
+//read 回调函数
+void read_cb(struct bufferevent *bev, void *arg)
+{  
+  struct evbuffer* input = bufferevent_get_input(bev); //获取读evbuffer的情况
+  int n = evbuffer_get_length(input); //获取evbuffer中字节个数
 
- void run() {
-     sylar::TcpServer::ptr server(new MyTcpServer); // 内部依赖shared_from_this()，所以必须以智能指针形式创建对象
-     auto addr = sylar::Address::LookupAny("0.0.0.0:12345");
-     SYLAR_ASSERT(addr != nullptr);
-     std::vector<sylar::Address::ptr> addrs;
-     addrs.emplace_back(addr);
+  char buf[1024];
+  bufferevent_read(bev,buf,n); //从缓冲区中读走n个字节的数据
+  buf[n] = '\0';
+  printf("read message = %s",buf);
+  
+  bufferevent_write(bev,buf,n); //回射信息给client
+}
+//error回调函数
+void error_cb(struct bufferevent *bev, short event, void *arg)
+{
+  //通过传入参数bev找到socket fd
+  evutil_socket_t fd = bufferevent_getfd(bev);
+  //cout << "fd = " << fd << endl;
+  if (event & BEV_EVENT_TIMEOUT)
+  {
+    printf("Timed out\n"); //if bufferevent_set_timeouts() called
+  }
+  else if (event & BEV_EVENT_EOF)
+  {
+    printf("connection closed\n");
+  }
+  else if (event & BEV_EVENT_ERROR)
+  {
+    printf("some other error\n");
+  }
+  bufferevent_free(bev);
+}
+ 
+int main()
+{
+  evutil_socket_t listener;
+  listener = socket(AF_INET, SOCK_STREAM, 0);
+  evutil_make_listen_socket_reuseable(listener);
+  
+  struct sockaddr_in sin;
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = 0;
+  sin.sin_port = htons(LISTEN_PORT);
+  if (bind(listener, (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    perror("bind");
+    return 1;
+  }
+  if (listen(listener, 1000) < 0) {
+    perror("listen");
+    return 1;
+  }
+  
+  printf("Listening...\n");
+  
+  evutil_make_socket_nonblocking(listener);
+  
+  struct event_base *base = event_base_new();
+  //监听套接字listener可读事件：一旦有client连接，就会调用do_accept_cb回调函数
+  struct event *listen_event = event_new(base, listener, EV_READ | EV_PERSIST, do_accept_cb, (void*)base);
 
-     std::vector<sylar::Address::ptr> fails;
-     while (!server->bind(addrs, fails)) {
-        sleep(2);
-     }
+  event_add(listen_event, NULL);
+  event_base_dispatch(base);
 
-     // SYLAR_LOG_INFO(g_logger) << "bind success, " << server->toString();
-
-     server->start();
- }
-
- int main(int argc, char *argv[]) {
-     sylar::EnvMgr::GetInstance()->init(argc, argv);
-     sylar::Config::LoadFromConfDir(sylar::EnvMgr::GetInstance()->getConfigPath());
-
-    sylar::IOManager iom(1);
-     iom.schedule(&run);
-
-     return 0;
- }
+  return 0;
+}
 
 #endif
